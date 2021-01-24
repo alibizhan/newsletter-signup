b4e9dbd87ed2cc1db2bf281ac05609f3fd71dac5
Ali Blau <alibizhan.samim@gmail.com>
2021-01-24 16:00:36 +0300

    change port

--- app.js
+++ app.js
@@ -61,6 +61,6 @@ app.post("/failure", function(req, res){
 
 
 
-app.listen(3000 || process.env.PORT, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("server is running on port 3000");
 });
